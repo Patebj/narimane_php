971aabcc6a37d67bd597805742ea57abf6a27153
Patebj <narimanehamiche4@gmail.com>
2025-01-15 22:58:11 +0100

    filmoteca Narimane

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -11,62 +11,101 @@ use App\Repository\FilmRepository;
 class FilmController
 {
     private TemplateRenderer $renderer;
+    private FilmRepository $filmRepository;
 
     public function __construct()
     {
         $this->renderer = new TemplateRenderer();
+        $this->filmRepository = new FilmRepository();
+    }
+
+    private function isPostRequest(): bool
+    {
+        return $_SERVER['REQUEST_METHOD'] === 'POST';
+    }
+
+    private function sanitize(string $input): string
+    {
+        return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    }
+
+    private function populateFilm(Film $film, array $data): Film
+    {
+        return $film->setTitle($this->sanitize($data['title'] ?? ''))
+                    ->setYear($this->sanitize($data['year'] ?? null))
+                    ->setType($this->sanitize($data['type'] ?? ''))
+                    ->setSynopsis($this->sanitize($data['synopsis'] ?? null))
+                    ->setDirector($this->sanitize($data['director'] ?? null));
+    }
+
+    private function renderNotFound(): void
+    {
+        echo $this->renderer->render('errors/404.html.twig');
+        exit();
     }
 
     public function list(array $queryParams)
     {
-        $filmRepository = new FilmRepository();
-        $films = $filmRepository->findAll();
-
-        /* $filmEntities = [];
-        foreach ($films as $film) {
-            $filmEntity = new Film();
-            $filmEntity->setId($film['id']);
-            $filmEntity->setTitle($film['title']);
-            $filmEntity->setYear($film['year']);
-            $filmEntity->setType($film['type']);
-            $filmEntity->setSynopsis($film['synopsis']);
-            $filmEntity->setDirector($film['director']);
-            $filmEntity->setCreatedAt(new \DateTime($film['created_at']));
-            $filmEntity->setUpdatedAt(new \DateTime($film['updated_at']));
-
-            $filmEntities[] = $filmEntity;
-        } */
-
-        //dd($films);
-
-        echo $this->renderer->render('film/list.html.twig', [
-            'films' => $films,
-        ]);
-
-        // header('Content-Type: application/json');
-        // echo json_encode($films);
+        $films = $this->filmRepository->findAll();
+        echo $this->renderer->render('film/list.html.twig', ['films' => $films]);
     }
 
-    public function create()
+    public function create() : void
     {
-        echo "Création d'un film";
+        if ($this->isPostRequest()) {
+            $film = $this->populateFilm(new Film(), $_POST);
+            $film->setCreatedAt(new \DateTime());
+            $this->filmRepository->createFilm($film);
+
+            header('Location: /film/list');
+            exit();
+        }
+
+        echo $this->renderer->render('film/create.html.twig');
     }
 
-    public function read(array $queryParams)
+    public function delete(array $queryParams) : void
     {
-        $filmRepository = new FilmRepository();
-        $film = $filmRepository->find((int) $queryParams['id']);
+        if ($this->isPostRequest()) {
+            $film = $this->filmRepository->find((int) $queryParams['id']);
+            if ($film) {
+                $this->filmRepository->deleteFilm($film);
+                header('Location: /film/list');
+                exit();
+            } else {
+                $this->renderNotFound();
+            }
+        }
 
-        dd($film);
+        echo $this->renderer->render('film/delete.html.twig', ['id' => $queryParams['id']]);
     }
 
-    public function update()
+    public function update(array $queryParams) : void
     {
-        echo "Mise à jour d'un film";
+        $film = $this->filmRepository->find((int) $queryParams['id']);
+        if (!$film) {
+            $this->renderNotFound();
+        }
+
+        if ($this->isPostRequest()) {
+            $film = $this->populateFilm($film, $_POST);
+            $film->setUpdatedAt(new \DateTime());
+            $this->filmRepository->updateFilm($film);
+
+            header('Location: /film/list');
+            exit();
+        }
+
+        echo $this->renderer->render('film/update.html.twig', ['film' => $film]);
     }
 
-    public function delete()
+    public function read(array $queryParams) : void
     {
-        echo "Suppression d'un film";
+        $film = $this->filmRepository->find((int) $queryParams['id']);
+        if ($film) {
+            echo $this->renderer->render('film/read.html.twig', ['film' => $film]);
+        } else {
+            $this->renderNotFound();
+        }
     }
 }
--- src/Repository/FilmRepository.php
+++ src/Repository/FilmRepository.php
@@ -7,49 +7,87 @@ namespace App\Repository;
 use App\Core\DatabaseConnection;
 use App\Service\EntityMapper;
 use App\Entity\Film;
+use PDO;
 
 class FilmRepository
 {
-    private \PDO $db; // Instance de connexion à la base de données
-    private EntityMapper $entityMapperService; // Service pour mapper les entités
+    private PDO $db;
+    private EntityMapper $entityMapperService;
 
     public function __construct()
     {
-        // Initialise la connexion à la base de données en utilisant DatabaseConnection
         $this->db = DatabaseConnection::getConnection();
-        // Initialise le service de mappage des entités
         $this->entityMapperService = new EntityMapper();
     }
 
-    // Méthode pour récupérer tous les films de la base de données
     public function findAll(): array
     {
-        // Requête SQL pour sélectionner tous les films
         $query = 'SELECT * FROM film';
-        // Exécute la requête et récupère le résultat
         $stmt = $this->db->query($query);
-
-        // Récupère tous les films sous forme de tableau associatif
         $films = $stmt->fetchAll();
-
-        // Utilise le service de mappage pour convertir les résultats en objets Film
         return $this->entityMapperService->mapToEntities($films, Film::class);
     }
 
-    // Méthode pour récupérer un film par son identifiant
     public function find(int $id): Film
     {
-        // Requête SQL pour sélectionner un film par son identifiant
         $query = 'SELECT * FROM film WHERE id = :id';
-        // Prépare la requête pour éviter les injections SQL
         $stmt = $this->db->prepare($query);
-        // Exécute la requête avec l'identifiant fourni
         $stmt->execute(['id' => $id]);
-
-        // Récupère le film sous forme de tableau associatif
         $film = $stmt->fetch();
-
-        // Utilise le service de mappage pour convertir le résultat en objet Film
         return $this->entityMapperService->mapToEntity($film, Film::class);
+    }
+
+    private function executeQuery(string $query, array $params = []): void
+    {
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($params);
+    }
+
+    public function createFilm(Film $film): void
+    {
+        $sql = "INSERT INTO film (title, year, type, synopsis, director, created_at, updated_at) 
+                VALUES (:title, :year, :type, :synopsis, :director, :createdAt, :updatedAt)";
+        
+        $params = [
+            'title' => $film->getTitle(),
+            'year' => $film->getYear(),
+            'type' => $film->getType(),
+            'synopsis' => $film->getSynopsis(),
+            'director' => $film->getDirector(),
+            'createdAt' => $film->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updatedAt' => $film->getUpdatedAt() ? $film->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+        ];
+
+        $this->executeQuery($sql, $params);
+    }
+
+    public function deleteFilm(Film $film): void
+    {
+        $sql = "DELETE FROM film WHERE id = :id";
+        $this->executeQuery($sql, ['id' => $film->getId()]);
+    }
+
+    public function updateFilm(Film $film): void
+    {
+        $sql = "UPDATE film 
+                SET title = :title, 
+                    year = :year, 
+                    type = :type, 
+                    synopsis = :synopsis, 
+                    director = :director, 
+                    updated_at = :updatedAt 
+                WHERE id = :id";
+        
+        $params = [
+            'id' => $film->getId(),
+            'title' => $film->getTitle(),
+            'year' => $film->getYear(),
+            'type' => $film->getType(),
+            'synopsis' => $film->getSynopsis(),
+            'director' => $film->getDirector(),
+            'updatedAt' => $film->getUpdatedAt()->format('Y-m-d H:i:s'),
+        ];
+
+        $this->executeQuery($sql, $params);
     }
 }
